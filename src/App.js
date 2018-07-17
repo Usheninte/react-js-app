619a56e162913590d1af7e5c3a3570ecd5106885
Usheninte <jrandangana@gmail.com>
2018-07-17 12:37:58 +0100

    knowledge++ in React coding

--- src/App.js
+++ src/App.js
@@ -17,15 +17,22 @@ class App extends Component {
     var finesse = {
       textAlign: 'center'
     };
+    
+    var salutation = ['Welcome', 'To', 'My', 'React App'].map(function(greet) {
+      return <p key={greet}>{greet}</p>;
+    });
 
     return (
       <div style={finesse}>
         <h1>Hello</h1>
-        <h3>My name is <Name /> </h3>
+        <br />
+        <h3>My name is</h3>
         {/* <button onClick={() => { console.log("I am Usheninte, The African!") }}> */}
         <button onClick={this.alertUser}>
-          <Name /> 
+          <h3><Name /></h3> 
         </button>
+        <br /><br /><br />
+        {salutation}
       </div>
     );
   
