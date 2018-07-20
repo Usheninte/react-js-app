550b74946fa22f6cde25e1ebfa891f573f3e4021
Usheninte <jrandangana@gmail.com>
2018-07-20 15:29:14 +0100

    learning about data and libraries

--- src/App.js
+++ src/App.js
@@ -17,15 +17,9 @@ class App extends Component {
     };
 
     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Hello World!</h1>
-      </header>
       <div style={finesse}>
         <Intro />
       </div>
-    </div>
     );
   
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Button } from 'react-bootstrap';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
